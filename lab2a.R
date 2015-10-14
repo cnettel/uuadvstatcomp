ea545530d14dd28258eb9c1196064e283a3bb237
corvusheretic <kalyanram.a@gmail.com>
2015-10-14 13:53:50 +0200

    Optimize f with plot

--- lab2a.R
+++ lab2a.R
@@ -2,3 +2,6 @@ f <- function (x) {(x - 3)^2 + 2 * (x - 3)^2 + 3 * (x - 15)^2 + sin(100*x)}
 optimize(f,c(0,15))
 optimize(f,c(9,12))
 optimize(f,c(10,11))
+
+xx <- seq(8,12,length=50)
+plot(xx,f(xx))
