b8edbbe0ed3c86e795799f33431f6aedd346ca4c
MiaXYZ <xzh@NB-21HDPX1.ad.du.se>
2015-10-18 20:29:30 +0200

    Currently i have two problems unsolved.

--- lab2a.R
+++ lab2a.R
@@ -18,6 +18,12 @@ optimise(SumFun, c(10,11))
 
 ### 3. Integrating a function ###
 
+###############################
+#                             #
+# I have PROBLEM here!!!      #
+#                             #
+###############################
+
 Fun1 <- function(x) {
   x*sin(x)
 }
@@ -25,10 +31,132 @@ Fun1 <- function(x) {
 Integral <- integrate(Fun1, lower = -7e5, upper = 7e5,
           subdivisions = 1e7)
 
-Integral
-
 system.time (integrate(Fun1, lower = -7e5, upper = 7e5,
                        subdivisions = 1e7))
 
+library(parallel)
 
-### 9. Getting a SUPR account (Done! xzh@du.se) ###
+cl <- makePSOCKcluster(2)
+
+system.time(parLapply(cl, c(-5e5, 5e5), (integrate(Fun1, lower = -7e5, upper = 7e5,
+                                          subdivisions = 1e7))))
+
+### 4. Functional Operators ###
+
+   # Memoisation #
+library(memoise)
+
+fib <- function(n) {
+  if (n < 2) return(1)
+  fib(n - 2) + fib(n - 1)
+}
+
+fib2 <- memoise(function(n) {
+  if (n < 2) return(1)
+  fib2(n - 2) + fib2(n - 1)
+})
+
+fib3 <- memoise(fib)
+
+system.time(fib(28))
+
+system.time(fib2(28))
+
+system.time(fib3(28))
+
+### 5. Domain-specific Languages ###
+
+    # qplot() #
+library(ggplot2)
+
+data(mpg, package= "ggplot2")
+
+attach (mpg)
+
+str(mpg)
+
+qplot(displ, hwy) #engine displacement (displ) and highway miles per gallon (hwy)#
+
+#plot the relation between displ and hwy for different factors of wheel drive using color=drv
+qplot(displ, hwy , color =drv) 
+
+#add statistics to the plot using geom (the following will add a smoother to a plot) 
+qplot(displ, hwy, geom=c("point","smooth")) 
+
+# check the linear relationship for the data:
+qplot(displ, hwy, geom=c("point","smooth"),method="lm")
+
+#check the linear relationship for different groups:
+qplot(displ, hwy, geom=c("point","smooth"),method="lm",color=drv)
+
+#one benefit of these plots is that you can modify them after they have been created.
+#For example, we can use theme() to modify theme setting. We assign the graph to variable:  
+gr <- qplot(displ, hwy, geom=c("point","smooth"),method="lm",color=drv)
+
+#Then we can modify it. For example change the color of the rectangular elements to pink:  
+gr + theme(panel.background = element_rect(fill = "pink"))
+
+
+
+##########################################
+#                                        #
+#          5. Exercise                   #
+#                                        #
+##########################################
+
+library(ggplot2)
+
+#1. Load the data set diamonds from ggplot2.
+data(diamonds)
+
+attach(diamonds)
+
+str(diamonds)
+
+#2. Look at the variables carat and price in the dataframe and 
+   #plot them against each other.
+qplot(carat, price)
+
+#3. Look at the relation between carat and price for different diamond colors.
+qplot(carat, price, color = color)
+
+#4. Add a smoother to the plot.
+qplot(carat, price,  geom=c("point","smooth"))
+
+#5. Change the title of legend of your plot to "New legends". 
+   #Tips: look at the theme() help to find out how to change the title of legend.
+egr <- qplot(carat, price, color = color) 
+# Set aesthetic of legend key
+
+# very low alpha value make it difficult to see legend key, 
+egr<- qplot(carat, price, colour = color,
+            alpha = I(1/100))
+
+#so set an appropriate value is important. 
+ #the following override.aes overwrites the alpha
+egr <- egr+guides(colour = guide_legend(override.aes = list(alpha = 1)))
+
+#egr + theme(legend.title = element_text(title ="New legends")) #Wrong
+
+#egr + scale_fill_discrete(name="New legends") # Wrong
+
+egr+ labs(color = "New legends") # Did the work correctlly
+
+#########################################################
+#
+# ??????? How can i do by using theme ????????
+#
+########################################################
+#6. Find out how to draw a boxplot to check 
+  #the distribution of price/carat for different colors.
+
+qplot(factor(color), price/carat, color= color, geom= c("boxplot", "jitter"))
+
+
+
+
+
+
+
+
+### 6. Getting a SUPR account (Done! Account is set up by using xzh@du.se) ###
