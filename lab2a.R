2c5f8107c54fe46034f7b0fe41e9691fc24590aa
agdosne <anndo252@FBV-38ZF6R1.user.uu.se>
2015-10-29 10:23:05 +0100

    parallel integration

--- lab2a.R
+++ lab2a.R
@@ -25,6 +25,34 @@ optimize(f=myfun, interval=c(-1000,1000))    # min: 8.71
 ### Integrate a function
 
 multsin <- function(x) {x*sin(x)} # create a new function
-
+integrate(multsin,lower=-7E5,upper=7E5,subdivisions=1E7) # result: 1356376
 system.time(integrate(multsin,lower=-7E5,upper=7E5,subdivisions=1E7)) # integrate it over large range and small steps
-# takes ~13 seconds
+# takes ~12 seconds
+
+library(parallel) # make it parallel
+p_multsin <- function(lim) { # create function that integrate multsin in parallel
+  y <- integrate(function(x) {x*sin(x)}, 
+                 lower = lim[1], 
+                 upper = lim[2], 
+                 subdivisions = 1E7)
+  return(y$value)
+}
+p_multsin(c(-7E5,7E5)) # test function: same result as before so OK
+
+p_integrate <- function(nodes) {
+  cl        <- makePSOCKcluster(nodes)
+  intervals <- seq(from = -7E5, to = 7E5, length.out = nodes)
+  min       <- intervals[-length(intervals)] # create vector of min values to use as lower arguments to integrate function
+  max       <- intervals[-1]                 # create vector of max values to use as upper arguments to integrate function
+  
+  out <- parLapply(cl = cl, X = Map(c,min,max), fun = p_multsin)
+  return(sum(unlist(out)))
+}
+
+p_integrate(2) # result: 1356376, same as before so function OK
+p_integrate(4) # same
+system.time({p_integrate(2)})  # takes 4 sec
+system.time({p_integrate(4)})  # takes 2.9 sec --> best
+system.time({p_integrate(8)})  # takes 3 sec
+# Speed up 4-fold using parallel computing on 4 nodes (from 12 to 3 seconds)
+
